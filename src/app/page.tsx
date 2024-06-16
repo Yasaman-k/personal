06a0e77d076281df90ab97a2c3424b47822f8109
Yasaman Karbasi <yasi_karbasi@yahoo.com>
2024-06-16 14:04:56 +0330

    feat: active animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ThreeScene from "./components/ThreeScene";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AnimatedLogo3D hover={false} boost={false} />
+      <AnimatedLogo3D hover={true} boost={true} />
       {/* <ThreeScene hover={false} boost={false} /> */}
     </main>
   );
