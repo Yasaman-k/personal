328cd2dddb24699de87825e888b62696ca8ec4cf
Yasaman Karbasi <work.yasaman.me@gmail.com>
2024-06-18 23:34:56 +0330

    feat: delete outdated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import AnimatedLogo3D from "./components/AnimatedLogo";
-import ThreeScene from "./components/ThreeScene";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <AnimatedLogo3D hover={true} boost={true} />
-      {/* <ThreeScene hover={false} boost={false} /> */}
     </main>
   );
 }
