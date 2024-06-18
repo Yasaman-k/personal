3b38ba709f93be349cea7ade73288b110fc9d879
Yasaman Karbasi <work.yasaman.me@gmail.com>
2024-06-19 01:32:14 +0330

    feat: coming soon v1

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (<>
+        <div>
+            <ul>
+                <li className="font-mono font-extrabold text-6xl z-10 text-white">Coming soon...</li>
+                <li></li>
+            </ul>
+            {/* <div className="bg-main-flower w-96 bg-red-400 h-96"></div> */}
+        </div>
+    </>)
+
+}
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import AnimatedLogo3D from "./components/AnimatedLogo";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AnimatedLogo3D hover={true} boost={true} />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24
+    bg-main-flower bg-blend-overlay bg-[length:100px_100px]  bg-repeat-space "  >
+      <div className="bg-gradient-to-r  from-[#15bb4dfd]  shadow-black shadow-2xl w-[700px] rounded-br-sm  rounded-tl-lg rounded-full h-[400px] text-center  content-center">
+        <Navbar />
+      </div>
+
+      {/* <AnimatedLogo3D hover={true} boost={true} /> */}
     </main>
   );
 }
